38684623a060379da4ab1c2feb5a084cabb936f8
Gustavo Galo <gg.gustavo.galo@gmail.com>
2025-03-04 05:34:46 -0300

    refactor: integrate login form with react-hook-form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import { AlertCircle, Loader2 } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,101 +9,126 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useSignIn } from "@/services/auth/sign-in";
-import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { Link, useRouter } from "@tanstack/react-router";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { toast } from "sonner";
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<"div">) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const { mutate, isPending, isError, error } = useSignIn({
-    onSuccess: (data) => {
-      localStorage.setItem("task-manager-token", data.token);
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
     },
   });
 
-  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const router = useRouter();
+  const { mutate, isPending } = useSignIn({
+    onSuccess: (data) => {
+      localStorage.setItem("task-manager-token", data.token);
+      toast.success("Successfully logged in!", {
+        description: "Redirecting you to your dashboard...",
+      });
+      router.navigate({ to: "/dashboard" });
+    },
+    onError: () => {
+      form.setError("email", {
+        message: "Invalid email or password.",
+      });
+      form.setError("password", {
+        message: "Invalid email or password.",
+      });
+      toast.error("Login failed", {
+        description: "Invalid email or password. Please try again.",
+      });
+    },
+    throwOnError: false,
+  });
 
+  async function onSubmit({ email, password }: z.infer<typeof formSchema>) {
     mutate({ email, password });
   }
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={onSubmit}>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  value={email}
-                  onChange={(event) => setEmail(event.currentTarget.value)}
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input
-                  value={password}
-                  onChange={(event) => setPassword(event.currentTarget.value)}
-                  type="password"
-                  required
-                />
-              </div>
-              {isError && (
-                <div className="flex items-center">
-                  <Alert variant="destructive">
-                    <AlertCircle className="h-4 w-4" />
-                    <AlertTitle>Error</AlertTitle>
-                    <AlertDescription>
-                      {error.response?.data.message}
-                    </AlertDescription>
-                  </Alert>
-                </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="your@email.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-              <Button disabled={isPending} type="submit" className="w-full">
-                {isPending ? (
-                  <>
-                    <Loader2 className="animate-spin" /> Please wait
-                  </>
-                ) : (
-                  "Login"
-                )}
-              </Button>
-              <Button variant="outline" className="w-full">
-                Login with Google
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="••••••••" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={isPending} className="w-full">
+              {isPending ? (
+                <>
+                  <Loader2 className="animate-spin" /> Please wait
+                </>
+              ) : (
+                "Login"
+              )}
+            </Button>
           </form>
-        </CardContent>
-      </Card>
-    </div>
+        </Form>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link to="/signup" className="underline underline-offset-4">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
